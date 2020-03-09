ec1d4e3bc26207cfc81083590ad44a81f34973e8
Felix <felixmann00@gmail.com>
2020-03-09 16:04:29 +0100

    Client und Server v01

--- src/Client.java
+++ src/Client.java
@@ -1,28 +1,74 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
-/**
- * @author Felix Mann
- * @version 1.0
- * @since 2020-März-09
- */
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Client {
-    public static void main (String []args) {
-        try {
-            Socket s = new Socket("localhost", 1999);
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            while(!br.readLine().equals("quit")) {
-                dos.writeUTF(br.readLine());
-                System.out.println("He says: " + dis.readUTF());
-                dos.flush();
+    private String serverAddress;
+    private PrintWriter out;
+    private JFrame frame = new JFrame("Chatter");
+    private JTextField textField = new JTextField(50);
+    private JTextArea messageArea = new JTextArea(16, 50);
+
+    private Client(String serverAddress) {
+        this.serverAddress = serverAddress;
+
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.SOUTH);
+        frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
+        frame.pack();
+
+        textField.addActionListener(e -> {
+            out.println(textField.getText());
+            textField.setText("");
+        });
+    }
+
+    private String getName() {
+        return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private void run() throws IOException {
+        try {
+            var socket = new Socket(serverAddress, 59001);
+            Scanner in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (in.hasNextLine()) {
+                var line = in.nextLine();
+                if (line.startsWith("SUBMITNAME")) {
+                    out.println(getName());
+                } else if (line.startsWith("NAMEACCEPTED")) {
+                    this.frame.setTitle("Chatter - " + line.substring(13));
+                    textField.setEditable(true);
+                } else if (line.startsWith("MESSAGE")) {
+                    messageArea.append(line.substring(8) + "\n");
+                }
             }
-            s.close();
-        } catch (IOException ie) {
-            ie.printStackTrace();
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
+        }
+        var client = new Client(args[0]);
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,87 @@
-/**
- * @author Felix Mann
- * @version 1.0
- * @since 2020-März-09
- */
-
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.Executors;
 
 public class Server {
-    public static void main (String []args) {
-        try {
-            ServerSocket ss = new ServerSocket(1999);
-            Socket s = ss.accept();
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            while(!br.readLine().equals("quit")) {
-                System.out.println("She says: " + dis.readUTF());
-                dos.writeUTF(br.readLine());
+
+    private static final Set<String> names = new HashSet<>();
+    private static Set<PrintWriter> writers = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running...");
+        var pool = Executors.newFixedThreadPool(500);
+        try (var listener = new ServerSocket(59001)) {
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
             }
-            ss.close();
-        } catch (Exception ie) {
-            ie.printStackTrace();
+        }
+    }
+
+    private static class Handler implements Runnable {
+        private String name;
+        private Socket socket;
+        private PrintWriter out;
+
+        Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                Scanner in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream(), true);
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.nextLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!name.isBlank() && !names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                out.println("NAMEACCEPTED " + name);
+                for (PrintWriter writer : writers) {
+                    writer.println("MESSAGE " + name + " has joined");
+                }
+                writers.add(out);
+
+                while (true) {
+                    String input = in.nextLine();
+                    if (input.toLowerCase().startsWith("/quit")) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                if (out != null) {
+                    writers.remove(out);
+                }
+                if (name != null) {
+                    System.out.println(name + " is leaving");
+                    names.remove(name);
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + " has left");
+                    }
+                }
+                try {
+                    socket.close();
+                } catch (IOException ignored) {
+                }
+            }
         }
     }
 }
-
