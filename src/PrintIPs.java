8153b869ddef93418c03878fee368e39a070cb25
Waldtelefon <gergoedusza@gmail.com>
2020-03-09 10:16:21 +0100

    try

---
+++ src/PrintIPs.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * @author Gergö Dusza
+ * @version 1.0
+ * @since 2020-März-09
+ */
+
+public class PrintIPs {
+    public static void main(String args[]) throws IOException {
+        String ipSub="172.16.9";
+        int timeout=1500;
+        for (int i=1;i<255;i++){
+            String hostIP=ipSub + "." + i;
+            if (InetAddress.getByName(hostIP).isReachable(timeout)){
+                System.out.println(hostIP);
+            }
+        }
+    }
+
+}
